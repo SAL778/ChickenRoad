e77ed386ee0b54f5013580ad57137aad305fea3c
Brendon Leenheer <bleenheer@gmail.com>
2024-01-27 23:38:38 -0700

    Car Lifetimee & two new colours

--- Assets/Scripts/car.cs
+++ Assets/Scripts/car.cs
@@ -13,21 +13,32 @@ public class Car : MonoBehaviour
     public float moveSpeed = 1f;
     public int length = 2;
 
+    [SerializeField]
     bool carWasVisible;
 
-    MeshRenderer meshRenderer;
+    bool lifetimeEnabled = false;
+    public float remainingLifeTime = 20f;
+
+    Renderer meshRenderer;
     Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        meshRenderer = GetComponent<MeshRenderer>();
+        //meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer = GetComponentInChildren<Renderer>();
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(remainingLifeTime > 0) lifetimeEnabled = true;
+        if(lifetimeEnabled)
+        {
+            remainingLifeTime -= Time.deltaTime;
+            if (remainingLifeTime < 0) Destroy(gameObject);
+        }
         //transform.position = transform.position += transform.forward * moveSpeed * Time.deltaTime;
         rb.MovePosition(transform.position += transform.forward * moveSpeed * Time.deltaTime);
         //Debug.Log("Car has lived!");
@@ -35,15 +46,16 @@ public class Car : MonoBehaviour
 
     private void LateUpdate()
     {
-        //Debug.Log("Car is visible? " + meshRenderer.isVisible);
+        Debug.Log("Car is visible? " + meshRenderer.isVisible);
         //This check prevents the car from being despawned if it spawned off screen.
         if(!carWasVisible)
         {
             if(meshRenderer.isVisible) carWasVisible = true;
         } else
         {
-            if (!GetComponent<MeshRenderer>().isVisible) Destroy(gameObject);
+            if (!meshRenderer.isVisible) Destroy(gameObject);
         }
+        
     }
 
     private void OnCollisionEnter(Collision collision)
